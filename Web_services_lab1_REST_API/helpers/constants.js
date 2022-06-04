5c3949c528512dc5571fa8cc6ad9ce6ec336e829
Amany Bahaa Eldin <amany.mohamed98@eng-st.cu.edu.eg>
2022-06-04 02:40:15 +0200

    Modified lab_1 folder

--- Web_services_lab1_REST_API/helpers/constants.js
+++
@@ -1,8 +0,0 @@
-const hateos = {
-    getBlogsHateos: (scheme, hostname) => ({
-        'self_url': `${scheme}${hostname}/blogs`,
-        'blog_url':  `${scheme}${hostname}/blogs/{blog_id}`,
-    })
-}
-
-module.exports = hateos
